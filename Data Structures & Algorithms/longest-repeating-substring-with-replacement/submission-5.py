7121524be81e320c95851efb987318aa38448f55
Shervin Naini <ryannaini@gmail.com>
2026-08-27 18:04:01 -0700

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s, k):
+        dic = [0] * 26
+        res = 1
+        l,r = 0,1
+
+        dic[ord(s[l]) - ord('A')] += 1
+
+        while r < len(s):
+            idx = ord(s[r]) - ord('A')
+            dic[idx] += 1 
+
+            if ((r - l + 1) - max(dic)) <= k:
+                res = max(res, (r-l + 1))
+            else:
+                while ((r - l) + 1) - max(dic) > k:
+                    dic[ord(s[l]) - ord('A')] -= 1
+                    l += 1
+            r += 1
+        
+        return res
+
+        
